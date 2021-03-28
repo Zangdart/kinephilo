ef4ba558c9c654c5b49fb1298f3f6e73d82f103e
Davy Castanet <davy.castanet@gmail.com>
2021-03-28 18:19:41 -0400

    added scroll effect and touch up first 2 pages

---
+++ js/scroll.js
@@ -0,0 +1,6 @@
+// ScrollReveal({ reset: true });
+ScrollReveal().reveal('.tm-welcome', { delay: 500 });
+ScrollReveal().reveal('.call-top-action', { delay: 200 });
+ScrollReveal().reveal('.footerScroll', { delay: 200 });
+
+
