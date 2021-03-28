61f0c1d7659a7e543da5585258631e44975f3607
Davy Castanet <davy.castanet@gmail.com>
2021-03-28 18:55:56 -0400

    remove ws

--- js/scroll.js
+++ js/scroll.js
@@ -1,7 +1,6 @@
 // ScrollReveal({ reset: true });
 ScrollReveal().reveal('.container-fluid', { delay: 300 });
 ScrollReveal().reveal('.tm-welcome', { delay: 500 });
-
 ScrollReveal().reveal('.slowScroll', { delay: 1200 });
 ScrollReveal().reveal('.call-top-action', { delay: 200 });
 ScrollReveal().reveal('.footerScroll', { delay: 200 });
